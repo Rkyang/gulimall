7cd5ba626249ab03fe7eaf6f053bb5b211243aa9
rkyang <rkyang@outlook.com>
2022-08-23 16:08:41 +0800

    项目初始架构构建

---
+++ gulimall-member/src/test/java/com/rkyang/gulimall/member/GulimallMemberApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rkyang.gulimall.member;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallMemberApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
