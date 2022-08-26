8033d3da3d5a8d341a4bfb4a4fefcb10fc812eaa
rkyang <rkyang@outlook.com>
2022-08-26 11:56:49 +0800

    增加gateway网关服务模块

---
+++ gulimall-gateway/src/main/java/com/rkyang/gulimall/gateway/GulimallGatewayApplication.java
@@ -0,0 +1,17 @@
+package com.rkyang.gulimall.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@EnableDiscoveryClient
+// 排除数据库连接相关依赖配置
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+public class GulimallGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GulimallGatewayApplication.class, args);
+    }
+
+}
