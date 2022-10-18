795ae0b5c4de786c8521447cedf3cb92c41ce818
rkyang <rkyang@outlook.com>
2022-10-18 15:37:35 +0800

    配置并测试使用alibaba cloud oss向阿里云oss上传文件

--- gulimall-product/src/test/java/com/rkyang/gulimall/product/GulimallProductApplicationTests.java
+++ gulimall-product/src/test/java/com/rkyang/gulimall/product/GulimallProductApplicationTests.java
@@ -1,5 +1,6 @@
 package com.rkyang.gulimall.product;
 
+import com.aliyun.oss.*;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.rkyang.gulimall.product.entity.BrandEntity;
 import com.rkyang.gulimall.product.service.BrandService;
@@ -7,6 +8,9 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
 import java.util.List;
 
 @SpringBootTest
@@ -14,6 +18,9 @@ class GulimallProductApplicationTests {
 
     @Autowired
     private BrandService brandService;
+
+    @Autowired
+    private OSSClient ossClientAlibaba;
 
     @Test
     void contextLoads() {
@@ -29,4 +36,58 @@ class GulimallProductApplicationTests {
         list.forEach(System.out::println);
     }
 
+    @Test
+    void testUploadOSS() {
+        // Endpoint以华东1（杭州）为例，其它Region请按实际情况填写。
+        String endpoint = "oss-cn-beijing.aliyuncs.com";
+        // 阿里云账号AccessKey拥有所有API的访问权限，风险很高。强烈建议您创建并使用RAM用户进行API访问或日常运维，请登录RAM控制台创建RAM用户。
+        String accessKeyId = "LTAI5tC59PNSjawqffAb96bx";
+        String accessKeySecret = "jN4is0pg7RIcbDF6d8yHVErUVEIyoX";
+        // 填写Bucket名称，例如examplebucket。
+        String bucketName = "rkyang-gulimall";
+        // 填写Object完整路径，完整路径中不能包含Bucket名称，例如exampledir/exampleobject.txt。
+        String objectName = "test1.jpg";
+        // 填写本地文件的完整路径，例如D:\\localpath\\examplefile.txt。
+        // 如果未指定本地路径，则默认从示例程序所属项目对应本地路径中上传文件流。
+        String filePath= "C:\\Users\\RKyan\\Pictures\\test.jpg";
+
+        // 创建OSSClient实例。
+        OSS ossClient = new OSSClientBuilder().build(endpoint, accessKeyId, accessKeySecret);
+
+        try {
+            InputStream inputStream = new FileInputStream(filePath);
+            // 创建PutObject请求。
+            ossClient.putObject(bucketName, objectName, inputStream);
+        } catch (OSSException oe) {
+            System.out.println("Caught an OSSException, which means your request made it to OSS, "
+                    + "but was rejected with an error response for some reason.");
+            System.out.println("Error Message:" + oe.getErrorMessage());
+            System.out.println("Error Code:" + oe.getErrorCode());
+            System.out.println("Request ID:" + oe.getRequestId());
+            System.out.println("Host ID:" + oe.getHostId());
+        } catch (ClientException ce) {
+            System.out.println("Caught an ClientException, which means the client encountered "
+                    + "a serious internal problem while trying to communicate with OSS, "
+                    + "such as not being able to access the network.");
+            System.out.println("Error Message:" + ce.getMessage());
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } finally {
+            if (ossClient != null) {
+                ossClient.shutdown();
+            }
+        }
+        System.out.println("上传至OSS成功……");
+    }
+
+    @Test
+    void testUploadOSSByAlibaba() {
+        try {
+            InputStream inputStream = new FileInputStream("C:\\Users\\RKyan\\Pictures\\minesweeper 100.jpg");
+            ossClientAlibaba.putObject("rkyang-gulimall", "test2.jpg", inputStream);
+            System.out.println("使用alibaba cloud oss上传成功……");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
