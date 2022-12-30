48c1b280c960f4ce8661b7117446ede455577ff2
rkyang <rkyang@outlook.com>
2022-12-30 14:45:59 +0800

    1、增加仓库服务网关； 2、完善仓库维护页面检索功能；

--- gulimall-ware/src/main/java/com/rkyang/gulimall/ware/service/impl/WareInfoServiceImpl.java
+++ gulimall-ware/src/main/java/com/rkyang/gulimall/ware/service/impl/WareInfoServiceImpl.java
@@ -1,16 +1,17 @@
 package com.rkyang.gulimall.ware.service.impl;
 
-import org.springframework.stereotype.Service;
-import java.util.Map;
+import com.alibaba.nacos.common.utils.StringUtils;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.rkyang.common.utils.PageUtils;
 import com.rkyang.common.utils.Query;
-
 import com.rkyang.gulimall.ware.dao.WareInfoDao;
 import com.rkyang.gulimall.ware.entity.WareInfoEntity;
 import com.rkyang.gulimall.ware.service.WareInfoService;
+import org.springframework.stereotype.Service;
+
+import java.util.Map;
 
 
 @Service("wareInfoService")
@@ -18,10 +19,17 @@ public class WareInfoServiceImpl extends ServiceImpl<WareInfoDao, WareInfoEntity
 
     @Override
     public PageUtils queryPage(Map<String, Object> params) {
-        IPage<WareInfoEntity> page = this.page(
-                new Query<WareInfoEntity>().getPage(params),
-                new QueryWrapper<WareInfoEntity>()
-        );
+        QueryWrapper<WareInfoEntity> queryWrapper = new QueryWrapper<>();
+
+        String key = (String) params.get("key");
+        if (StringUtils.isNotEmpty(key)) {
+            queryWrapper.and(qw -> qw.like("id", key)
+                    .or().like("name", key)
+                    .or().like("address", key)
+                    .or().like("areacode", key));
+        }
+
+        IPage<WareInfoEntity> page = this.page(new Query<WareInfoEntity>().getPage(params), queryWrapper);
 
         return new PageUtils(page);
     }
